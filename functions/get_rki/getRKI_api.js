e864dca5f10d38b62b60624758c01cf0bdabe0ae
BolverBlitz <35345288+BolverBlitz@users.noreply.github.com>
2020-03-28 07:22:22 +0100

    Added .catch

--- functions/get_rki/getRKI_api.js
+++ functions/get_rki/getRKI_api.js
@@ -2,25 +2,43 @@ var RKIurl = 'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services
 const request = require("request");
 const util = require('util');
 
+function IsJsonString(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
 let getRKI = function getCorona() {
     return new Promise(function(resolve, reject) {
         request(RKIurl, (err, res, body) => {
-            //let Output = body.split(",");
-            const obj = JSON.parse(body)
-            resolve(obj);
+
+            if(err){reject(err)}
+
+            if(IsJsonString(body)){
+                const obj = JSON.parse(body)
+                if(obj.error !== undefined){
+                    reject(obj.error)
+                }else{
+                    resolve(obj); 
+                }
+            }else{
+                reject("Not Json")
+            }
         })
     })
 }
 
 getRKI().then(function(RKI) {
-    //console.log(RKI.features)
     RKI.features.map((Werte) => {
         let Grammatik = ['sind','sind'];
         if(Werte.attributes.cases === 1){Grammatik[0] = "ist";}
         if(Werte.attributes.deaths === 1){Grammatik[1] = "ist";}
         console.log("In " + Werte.attributes.BL + " (" + Werte.attributes.county + ") " + Grammatik[0] + " " + Werte.attributes.cases + " infiziert, dass sind " + Werte.attributes.cases_per_100k + " pro 100.000 Einwohner.\nDort " + Grammatik[1] + " " + Werte.attributes.deaths + " gestorben, dass macht eine Todesrate von " + Math.round(Werte.attributes.death_rate * 100) / 100 + "%\n\n")
     });
-});
+}).catch(error => console.log('Error:', error));
 
 module.exports = {
 	getRKI
